155fa73407ba9476620206ee256b1535be9ccb2c
Jakub Wegrzyn <jakubek.weg@gmail.com>
2022-11-17 19:10:51 +0100

    Hopefully make teams work again

--- bot/src/logic-teams.ts
+++ bot/src/logic-teams.ts
@@ -1,169 +1,261 @@
-import { Page } from "puppeteer"
-import { MS_TEAMS_CREDENTIALS_LOGIN, MS_TEAMS_CREDENTIALS_ORIGINS, MS_TEAMS_CREDENTIALS_PASSWORD } from "./config"
-import Session from "./session"
-import { sleep } from "./utils"
+import { Page } from "puppeteer";
+import {
+  MS_TEAMS_CREDENTIALS_LOGIN,
+  MS_TEAMS_CREDENTIALS_ORIGINS,
+  MS_TEAMS_CREDENTIALS_PASSWORD,
+} from "./config";
+import Session from "./session";
+import { sleep } from "./utils";
 
 const requireOriginForCredentials = (page: Page) => {
-    const url = page.url()
-    const origin = new URL(url).origin;
-    if (!MS_TEAMS_CREDENTIALS_ORIGINS.includes(origin))
-        throw new Error(`Origin ${origin} not permitted to enter credentials`)
-}
+  const url = page.url();
+  const origin = new URL(url).origin;
+  if (!MS_TEAMS_CREDENTIALS_ORIGINS.includes(origin))
+    throw new Error(`Origin ${origin} not permitted to enter credentials`);
+};
 
 export const startTeamsSession = async (url: string, session: Session) => {
-    const page = session.page
+  const page = session.page;
 
-    session.assertActive()
-    await page.browserContext().overridePermissions(url, ['microphone', 'camera'])
-    await page.goto(url, { waitUntil: "networkidle2" })
-    session.assertActive()
+  session.assertActive();
+  await page
+    .browserContext()
+    .overridePermissions(url, ["microphone", "camera"]);
+  await page.goto(url, { waitUntil: "networkidle2" });
+  session.assertActive();
 
-    let willLogin = true
+  let willLogin = true;
+  try {
+    await page.waitForSelector("input[type=email]");
+  } catch (e) {
+    willLogin = false;
+    await page.waitForSelector("#ts-waffle-button");
+  }
+  if (willLogin) {
+    await page.waitForSelector("input[type=email]");
+    requireOriginForCredentials(page);
+    await page.type("input[type=email]", MS_TEAMS_CREDENTIALS_LOGIN!, {
+      delay: 20,
+    });
+    await page.keyboard.press("Enter", { delay: 100 });
+    await page.waitForSelector("input[type=password]", {
+      timeout: 20_000,
+      hidden: true,
+    });
+    await page.waitForSelector("input[type=password]", { timeout: 20_000 });
+    await page.waitForSelector("input[type=text]", { timeout: 20_000 });
+
+    session.assertActive();
+    await page.evaluate(
+      () =>
+        ((
+          (document.querySelector("input[type=email]") as HTMLInputElement) || {
+            value: "",
+          }
+        ).value = "")
+    );
+    requireOriginForCredentials(page);
     try {
-        await page.waitForSelector('input[type=email]')
-    } catch (e) {
-        willLogin = false
-        await page.waitForSelector('#ts-waffle-button')
-    }
-    if (willLogin) {
-        await page.waitForSelector('input[type=email]')
-        requireOriginForCredentials(page)
-        await page.type('input[type=email]', MS_TEAMS_CREDENTIALS_LOGIN!, { delay: 20 })
-        await page.keyboard.press('Enter', { delay: 100 })
-        await page.waitForSelector('input[type=password]', { timeout: 20_000, hidden: true })
-        await page.waitForSelector('input[type=password]', { timeout: 20_000, })
-        await page.waitForSelector('input[type=text]', { timeout: 20_000, })
-
-        session.assertActive()
-        await page.evaluate(() => (document.querySelector('input[type=email]') as HTMLInputElement || { value: '' }).value = '')
-        requireOriginForCredentials(page)
-        try { await page.type('input[type=email]', MS_TEAMS_CREDENTIALS_LOGIN!, { delay: 20 }) } catch (e) { }
-        requireOriginForCredentials(page)
-        try { await page.type('input[type=text]', MS_TEAMS_CREDENTIALS_LOGIN!, { delay: 20 }) } catch (e) { }
-        requireOriginForCredentials(page)
-        await page.type('input[type=password]', MS_TEAMS_CREDENTIALS_PASSWORD!, { delay: 20 })
-        await page.keyboard.press('Enter', { delay: 100 })
-        await page.waitForSelector('input[type=submit]', { hidden: true })
-        await page.waitForSelector('input[type=submit]', { timeout: 20_000 })
-        requireOriginForCredentials(page)
-        await page.click('input[type=submit]')
-        await sleep(10_000)
-        session.assertActive()
-    }
-
-    await page.goto(url, { waitUntil: "domcontentloaded" })
-    await sleep(3_000)
-
-    session.assertActive()
+      await page.type("input[type=email]", MS_TEAMS_CREDENTIALS_LOGIN!, {
+        delay: 20,
+      });
+    } catch (e) {}
+    requireOriginForCredentials(page);
     try {
-        await page.waitForSelector('button[type=button].icons-call-jump-in', { timeout: 60_000 })
+      await page.type("input[type=text]", MS_TEAMS_CREDENTIALS_LOGIN!, {
+        delay: 20,
+      });
+    } catch (e) {}
+    requireOriginForCredentials(page);
+    await page.type("input[type=password]", MS_TEAMS_CREDENTIALS_PASSWORD!, {
+      delay: 20,
+    });
+    await page.keyboard.press("Enter", { delay: 100 });
+    await page.waitForSelector("input[type=submit]", { hidden: true });
+    await page.waitForSelector("input[type=submit]", { timeout: 20_000 });
+    requireOriginForCredentials(page);
+    await page.click("input[type=submit]");
+    await sleep(10_000);
+    session.assertActive();
+  }
+
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  await sleep(3_000);
+
+  session.assertActive();
+  try {
+    await page.waitForSelector("button[type=button].icons-call-jump-in", {
+      timeout: 60_000,
+    });
+  } catch (e) {
+    console.warn("Failed to find join button, trying loading page again");
+
+    await page.goto("about:blank", { waitUntil: "domcontentloaded" });
+    await sleep(500);
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+
+    // await page.click('.btn-group.app-tabs-list-item:not(.app-tabs-selected)')
+    // await sleep(2000)
+    // await page.click('.btn-group.app-tabs-list-item:not(.app-tabs-selected)')
+    // await sleep(2000)
+    // await page.click('.start-meetup')
+    // await sleep(5000)
+    // await page.waitForSelector('#leave-calling-pre-join')
+    // await page.click('#leave-calling-pre-join')
+    // page.screenshot({ path: '/recordings/debug2.png', })
+
+    try {
+      await page.waitForSelector("button[type=button].icons-call-jump-in", {
+        timeout: 60_000,
+      });
     } catch (e) {
-        console.warn('Failed to find join button, trying loading page again');
+      console.warn("Failed to find join button, trying loading page again");
 
-        await page.goto('about:blank', { waitUntil: 'domcontentloaded' })
-        await sleep(500)
-        await page.goto(url, { waitUntil: "domcontentloaded" })
+      await page.goto("about:blank", { waitUntil: "domcontentloaded" });
+      await sleep(500);
+      await page.goto(url, { waitUntil: "domcontentloaded" });
 
-        // await page.click('.btn-group.app-tabs-list-item:not(.app-tabs-selected)')
-        // await sleep(2000)
-        // await page.click('.btn-group.app-tabs-list-item:not(.app-tabs-selected)')
-        // await sleep(2000)
-        // await page.click('.start-meetup')
-        // await sleep(5000)
-        // await page.waitForSelector('#leave-calling-pre-join')
-        // await page.click('#leave-calling-pre-join')
-        // page.screenshot({ path: '/recordings/debug2.png', })
+      await page.waitForSelector("button[type=button].icons-call-jump-in", {
+        timeout: 60_000,
+      });
+    }
+  }
+  await page.click("button[type=button].icons-call-jump-in");
 
-        try {
-            await page.waitForSelector('button[type=button].icons-call-jump-in', { timeout: 60_000 })
-        } catch (e) {
-            console.warn('Failed to find join button, trying loading page again');
+  let frame = page.frames().find((e) => e.name().includes("experience"));
+  while (!frame) {
+    await sleep(1000);
+    frame = page.frames().find((e) => e.name().includes("experience"));
+  }
 
-            await page.goto('about:blank', { waitUntil: 'domcontentloaded' })
-            await sleep(500)
-            await page.goto(url, { waitUntil: "domcontentloaded" })
+  await frame.waitForSelector("#prejoin-join-button", {
+    timeout: 10_000,
+  });
+  session.assertActive();
+  await frame.click("#prejoin-join-button");
 
-            await page.waitForSelector('button[type=button].icons-call-jump-in', { timeout: 60_000 })
+  try {
+    await frame.waitForSelector("#prejoin-join-button", {
+      timeout: 10_000,
+      hidden: true,
+    });
+  } catch (e) {
+    await frame.click("#prejoin-join-button");
+  }
+
+  const micButton = await frame.waitForSelector("#microphone-button", {
+    timeout: 60_000,
+  });
+  const needsMute = await micButton.evaluate(
+    (element) => !element.getAttribute("aria-label")?.includes("Unmute")
+  );
+  session.assertActive();
+  if (needsMute) await frame.click("#microphone-button");
+
+  sleep(1_000)
+    .then(() =>
+      frame.waitForSelector(
+        '[data-tid="callingAlertDismissButton_DeviceCaptureMute"]',
+        {
+          timeout: 0,
         }
-    }
-    await page.click('button[type=button].icons-call-jump-in')
+      )
+    )
+    .then(() =>
+      frame.click('[data-tid="callingAlertDismissButton_DeviceCaptureMute"]')
+    )
+    .catch((e) => void e);
 
-    await page.waitForSelector('button[type=button].join-btn', { timeout: 10_000 })
-    session.assertActive()
-    await page.click('button[type=button].join-btn')
+  sleep(1_000)
+    .then(() =>
+      frame.waitForSelector(
+        '[data-tid="callingAlertDismissButton_JoinersOngoingRecording"]',
+        {
+          timeout: 0,
+        }
+      )
+    )
+    .then(() =>
+      frame.click(
+        '[data-tid="callingAlertDismissButton_JoinersOngoingRecording"]'
+      )
+    )
+    .catch((e) => void e);
 
-    try {
-        await page.waitForSelector('button[type=button].join-btn', { timeout: 10_000, hidden: true })
-    } catch (e) {
-        await page.click('button[type=button].join-btn')
-    }
-
-    await page.waitForSelector('#microphone-button', { timeout: 60_000 })
-    session.assertActive()
-    await page.click('#microphone-button')
-
-    await page.waitForSelector('#callingButtons-showMoreBtn', { timeout: 10_000 })
-    session.assertActive()
-    await page.click('#callingButtons-showMoreBtn')
-
-    await page.waitForSelector('#full-screen-button', { timeout: 10_000 })
-    session.assertActive()
-    await page.click('#full-screen-button')
-
-    sleep(1_000)
-        .then(() => page.waitForSelector('.calling-alert-container .icons-close', { timeout: 0 }))
-        .then(() => page.click('.calling-alert-container .icons-close'))
-        .catch(e => void (e))
-}
+  sleep(10_000)
+    .then(async () => {
+      console.log(
+        await frame.evaluate(() =>
+          Array.from(document.querySelectorAll("[data-tid]")).map((e) =>
+            e.getAttribute("data-tid")
+          )
+        )
+      );
+    })
+    .catch((e) => void e);
+};
 
 export const observeMeetingClosedState = (page: Page) => {
-    let finished = false
-    let shouldBeClosed = false
+  const frame = page.frames().find((e) => e.name().includes("experience"));
+  let finished = false;
+  let shouldBeClosed = false;
 
-    sleep(1_000)
-        .then(async () => {
+  sleep(1_000)
+    .then(async () => {
+      (await frame.waitForSelector("#roster-button", { timeout: 0 })).click();
+      await sleep(100);
 
-            (await page.waitForSelector('#roster-button', { timeout: 0 })).click();
-            await sleep(50);
+      (await frame.waitForSelector("#roster-button", { timeout: 0 })).click();
 
-            (await page.waitForSelector('[title="Close right pane"]', { timeout: 0 })).click()
+      await frame.waitForSelector("#roster-title-section-2", { timeout: 0 });
 
-            await page.waitForSelector('[data-tid="participantsInCall"] .toggle-number', { timeout: 0 })
+      const getParticipantsCount = () =>
+        frame.evaluate(() => {
+          const text = document
+            .querySelector("#roster-title-section-2")
+            .textContent.replace("(", "")
+            .replace(")", "");
+          return parseInt(text.substring(1 + text.lastIndexOf(" ")), 10);
+        });
 
-            const getParticipantsCount = () => page.evaluate(() => parseInt(document.querySelector('[data-tid="participantsInCall"] .toggle-number').textContent.replace('(', '').replace(')', ''), 10))
+      let top = 0;
+      const LEAVE_WHEN_TOP_COUNT_BELOW = 0.4;
+      const MIN_TOP_PARTICIPANTS_TO_LEAVE = 5;
 
-            let top = 0
-            const LEAVE_WHEN_TOP_COUNT_BELOW = 0.4
-            const MIN_TOP_PARTICIPANTS_TO_LEAVE = 5
+      while (true) {
+        const participantsCount = await getParticipantsCount();
 
-            while (true) {
-                const participantsCount = await getParticipantsCount()
-
-                if (participantsCount > top) {
-                    top = participantsCount
-                } else if (top >= MIN_TOP_PARTICIPANTS_TO_LEAVE && participantsCount < top * LEAVE_WHEN_TOP_COUNT_BELOW) {
-                    // should leave
-                    shouldBeClosed = true
-                    return
-                }
-                if (finished || shouldBeClosed) return
-                await sleep(5_000)
-            }
-        })
-        .catch(e => console.error(e))
-
-    const originalUrl = page.url()
-    return {
-        checkStatus: async (page: Page) => {
-            if (shouldBeClosed)
-                return 'lost-participants'
-
-            if (await page.$('form[name="retryForm"]') || originalUrl !== page.url()) {
-                finished = true
-                return 'closed'
-            }
-
-            return null
+        if (participantsCount > top) {
+          top = participantsCount;
+        } else if (
+          top >= MIN_TOP_PARTICIPANTS_TO_LEAVE &&
+          participantsCount < top * LEAVE_WHEN_TOP_COUNT_BELOW
+        ) {
+          // should leave
+          shouldBeClosed = true;
+          return;
         }
-    }
-}
+        if (finished || shouldBeClosed) return;
+        await sleep(5_000);
+      }
+    })
+    .catch((e) => console.error(e));
+
+  const originalUrl = page.url();
+  return {
+    checkStatus: async (page: Page) => {
+      if (shouldBeClosed) return "lost-participants";
+
+      if (
+        (await page.$('form[name="retryForm"]')) ||
+        (await frame.$('form[name="retryForm"]')) ||
+        originalUrl !== page.url()
+      ) {
+        finished = true;
+        return "closed";
+      }
+
+      return null;
+    },
+  };
+};
